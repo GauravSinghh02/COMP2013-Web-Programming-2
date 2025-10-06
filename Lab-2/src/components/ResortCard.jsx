baffc08bbf2fd99db69042b1adc8b2a1ddca0605
Gaurav Singh <gauravsingh@Gauravs-MacBook-Air-M2.local>
2025-10-06 15:59:12 -0400

    Updated as per Lab Concepts

--- Lab-2/src/components/ResortCard.jsx
+++ Lab-2/src/components/ResortCard.jsx
@@ -1,20 +1,30 @@
-function ResortCard(props) {
-    const ratingStyle = props.rating > 4.0 ? 'rating-green' : 'rating-red';
+export default function ResortCard(props) {
+  console.log(props);
 
-    return (
-        <div className="card-component">
-            <img src={props.image} alt={props.hotel} />
-            <h3>{props.country}</h3>
-            <p>{props.hotel}</p>
-            <p>
-                <span className={ratingStyle}>{props.rating} ★</span>
-            </p>
-            <p>
-                <strong>${props.price}</strong>/night
-            </p>
-        </div>
-    );
+  return (
+    <div
+      className={
+        props.rating > 4.0
+          ? "rating-green"
+          : "rating-red"
+      }
+    >
+      <img src={props.image} alt="" width="100px" />
+      <h2>{props.hotel}</h2>
+      {props.rating > 4.0 ? (
+        <h3 className="Card-Sale">High Rating</h3>
+      ) : (
+        <h3>&nbsp;</h3>
+      )}
+      <p>{props.country}</p>
+      <p>
+        <strong>${props.price}</strong>/night
+      </p>
+      <button>Book Now</button>
+    </div>
+  );
 }
 
-export default ResortCard;
+
+
 
