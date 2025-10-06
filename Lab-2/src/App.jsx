f311c20fdf0c71c0b00262091c056b563af19edf
Gaurav Singh <gauravsingh@Gauravs-MacBook-Air-M2.local>
2025-10-06 18:54:04 -0400

    Updated as per Lecture Concepts and Topics covered

--- Lab-2/src/App.jsx
+++ Lab-2/src/App.jsx
@@ -1,26 +1,63 @@
-import './App.css'
-import ResortCard from './components/ResortCard'
-import img1 from './assets/images/1.jpg'
-import img2 from './assets/images/2.jpg'
-import img3 from './assets/images/3.jpg'
-import img4 from './assets/images/4.jpg'
-import img5 from './assets/images/5.jpg'
-import img6 from './assets/images/6.jpg'
+import "./App.css";
+import Card from "./components/ResortCard";
+import img1 from "./assets/images/1.jpg";
+import img2 from "./assets/images/2.jpg";
+import img3 from "./assets/images/3.jpg";
+import img4 from "./assets/images/4.jpg";
+import img5 from "./assets/images/5.jpg";
+import img6 from "./assets/images/6.jpg";
 
 function App() {
   return (
     <>
       <h1>Resorts Lite</h1>
+      <div className="divider" />
       <div className="main-container">
-        <ResortCard image={img1} country="Indonesia" hotel="Gili Air Hotel" rating={4.8} price={589} />
-        <ResortCard image={img2} country="Seychelles" hotel="Hilton Resort" rating={4.2} price={629} />
-        <ResortCard image={img3} country="US Virgin Islands" hotel="Goa Resort" rating={3.5} price={485} />
-        <ResortCard image={img4} country="Bahamas" hotel="Kuredu Resort" rating={4.1} price={729} />
-        <ResortCard image={img5} country="Mauritius" hotel="Trou d'eau Douce" rating={4.9} price={877} />
-        <ResortCard image={img6} country="Bermuda" hotel="Staniel Cay Hotel" rating={3.2} price={365} />
+        <Card
+          image={img1}
+          country="Indonesia"
+          name="Gili Air Hotel"
+          ratings={4.8}
+          price="$589/night"
+        />
+        <Card
+          image={img2}
+          country="Seychelles"
+          name="Hilton Resort"
+          ratings={4.2}
+          price="$629/night"
+        />
+        <Card
+          image={img3}
+          country="US Virgin Islands"
+          name="Goa Resort"
+          ratings={3.5}
+          price="$485/night"
+        />
+        <Card
+          image={img4}
+          country="Bahamas"
+          name="Kuredu Resort"
+          ratings={4.1}
+          price="$729/night"
+        />
+        <Card
+          image={img5}
+          country="Mauritius"
+          name="Trou d'eau Douce"
+          ratings={4.9}
+          price="$877/night"
+        />
+        <Card
+          image={img6}
+          country="Bermuda"
+          name="Staniel Cay Hotel"
+          ratings={3.2}
+          price="$365/night"
+        />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Lab-2/src/components/ResortCard.jsx
+++ Lab-2/src/components/ResortCard.jsx
@@ -1,30 +1,18 @@
 export default function ResortCard(props) {
-  console.log(props);
-
   return (
-    <div
-      className={
-        props.rating > 4.0
-          ? "rating-green"
-          : "rating-red"
-      }
-    >
-      <img src={props.image} alt="" width="100px" />
-      <h2>{props.hotel}</h2>
-      {props.rating > 4.0 ? (
-        <h3 className="Card-Sale">High Rating</h3>
-      ) : (
-        <h3>&nbsp;</h3>
-      )}
-      <p>{props.country}</p>
+    <div className="Card-Components">
+      <img src={props.image} alt="" width="200px" />
+      <h2>{props.country}</h2>
+      <p>{props.name}</p>
       <p>
-        <strong>${props.price}</strong>/night
+        {props.ratings > 4.0 ? (
+          <p className="ratings-green" >{props.ratings} ★  </p>
+        ) : (
+          <p className="ratings-red" >{props.ratings} ★  </p>
+        )}
       </p>
-      <button>Book Now</button>
+
+      <p>{props.price}</p>
     </div>
   );
 }
-
-
-
-
